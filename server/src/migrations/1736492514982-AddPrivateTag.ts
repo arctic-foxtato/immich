7ed58f1db586b959c7c0a7c2958a6125748872d8
David <david.brewin.hall@gmail.com>
2025-01-12 19:13:38 -0800

    updating default value to true on existing rows

--- server/src/migrations/1736492514982-AddPrivateTag.ts
+++ server/src/migrations/1736492514982-AddPrivateTag.ts
@@ -4,7 +4,7 @@ export class AddPrivateTag1736492514982 implements MigrationInterface {
     name = 'AddPrivateTag1736492514982'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tags" ADD "isPrivate" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "tags" ADD "isPrivate" boolean NOT NULL DEFAULT true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
